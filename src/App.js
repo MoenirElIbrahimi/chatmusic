fca0c0b11f733035f94b40ea64af423f9ccf4bd0
Void787 <1195841@student.roc-nijmegen.nl>
2023-05-24 15:40:17 +0200

    geprobeert de connectie met de database te testen (onsuccesful)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import "./database/database.js"
 
 function App() {
+
+  const [getTaskList, setTaskList] = useState([]);
+  const jobCollectionRef = collection(db, "task");
+
+  useEffect(()=>{
+    const getTasks = async () => {
+      const data = await getDocs(jobCollectionRef);
+      setTaskList(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    }
+    getTasks();
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
